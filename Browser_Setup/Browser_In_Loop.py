b2550e9154c3b880569087e1d033c57bd01dab34
Naagraaz <krishnamurari93km@gmail.com>
2025-04-22 06:34:37 +0530

    Selenium

--- Browser_Setup/Browser_In_Loop.py
+++ Browser_Setup/Browser_In_Loop.py
@@ -1,5 +1,8 @@
+import time
+
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
 
 # Create an Options object to customize the behavior of Chrome
 options = Options()
@@ -13,3 +16,14 @@ Browser_List=[webdriver.Chrome(options=options),webdriver.Firefox(),webdriver.Ed
 for driver in Browser_List:
     driver.maximize_window()
     driver.get("https://facebook.com/")
+
+    # Identify the element locater
+    driver.find_element(By.ID, "email").send_keys("Naagraaz@gmail.com")
+    pass_elem = driver.find_element(By.NAME, "pass")
+    pass_elem.clear()
+    pass_elem.send_keys("Naagraaz@123")
+
+    time.sleep(2)
+
+    driver.find_element(By.NAME, "login").click()
+    time.sleep(2)
--- Browser_Setup/Chrome_Setup.py
+++ Browser_Setup/Chrome_Setup.py
@@ -1,6 +1,9 @@
 # Importing required modules from Selenium
+import time
+
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
 
 # Create an Options object to customize the behavior of Chrome
 options = Options()
@@ -13,3 +16,16 @@ driver = webdriver.Chrome(options=options)
 
 # Open the Facbook website
 driver.get("https://facebook.com/")
+
+# Identify the element locater
+driver.find_element(By.ID,"email").send_keys("Naagraaz@gmail.com")
+pass_elem=driver.find_element(By.NAME,"pass")
+pass_elem.clear()
+pass_elem.send_keys("Naagraaz@123")
+
+time.sleep(2)
+
+driver.find_element(By.NAME,"login").click()
+time.sleep(2)
+
+
